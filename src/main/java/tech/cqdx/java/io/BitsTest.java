134f3910ea0ea8d7c39752c497cdca96aec09a20
王京东 <wangjingdong@MacBook-Pro.local>
2020-06-22 02:29:57 +0800

    a;

---
+++ src/main/java/tech/cqdx/java/io/BitsTest.java
@@ -0,0 +1,27 @@
+package tech.cqdx.java.io;
+
+public class BitsTest {
+
+    static boolean getBoolean(byte[] b, int off) {
+        return b[off] != 0;
+    }
+
+    static char getChar(byte[] b, int off) {
+        return (char) ((b[off + 1] & 0xFF) +
+                (b[off] << 8));
+    }
+
+    public static void main(String[] args) {
+        byte b = 'a';
+
+        for (int i = 0; i < 255; i++) {
+            System.out.println(i+"---"+(char) i);
+        }
+        byte[] barray = {1, 2, 3};
+
+//        System.out.println(b);
+        boolean aBoolean = BitsTest.getBoolean(barray, 1);
+        System.out.println(barray[1]);
+        System.out.println(aBoolean);
+    }
+}
